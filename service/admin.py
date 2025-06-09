5a5cc9753e75f75ae3f923fbdbaa8706c48dae0e
Alina Davydenko <Alinadavydenko620@gmail.com>
2025-06-09 15:41:17 +0300

    admin

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from service.models import Recipient, Message, Newsletter, Attempt
 
-# Register your models here.
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name', 'comment')
+    search_fields = ('email', 'full_name')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'body')
+    search_fields = ('subject',)
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'start_time', 'end_time', 'status', 'message')
+    list_filter = ('status',)
+    filter_horizontal = ('recipients',)
+
+
+@admin.register(Attempt)
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ('newsletter', 'attempt_time', 'status', 'server_response')
+    list_filter = ('status',)
+    
